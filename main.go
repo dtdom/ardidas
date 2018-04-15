03c8101866d3fe10a6eb4e0765249460e965c412
nov1kk <ivandelarubia@gmail.com>
2018-04-15 14:20:16 +0200

    update insta images each hour

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "github.com/dtdom/ardidas/config"
+import (
+	"strings"
+	"time"
+
+	"github.com/dtdom/ardidas/config"
+	"github.com/dtdom/ardidas/models"
+)
+
+func updateInstaImages() {
+	for true {
+		items := models.GetAllItems()
+
+		for _, item := range items {
+			images := ardidas_insta.GetValidImages(item.Hashtag)
+			item.ArrayInsta = strings.Join(images[:], ",")
+			item.Store()
+		}
+
+		time.Sleep(1 * time.Hour)
+	}
+}
 
 func main() {
+	go updateInstaImages()
+
 	r := Runner{}
 	r.Initialize()
 	r.Run(":" + config.MainConfig.Server.Port)
